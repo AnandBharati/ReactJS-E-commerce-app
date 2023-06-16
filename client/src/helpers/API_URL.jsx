cf6c1b995e41582f0dbf4dc73b462da82a166e28
AnandBharati <anandbharati0402@gmail.com>
2023-06-16 22:49:18 +0530

    checking env

--- client/src/helpers/API_URL.jsx
+++ client/src/helpers/API_URL.jsx
@@ -1,6 +1,3 @@
-import process from 'process'
-
-const apiUrl = process?.env?.APIURL
-//  || 'https://kind-rose-earthworm-hose.cyclic.app'
+const apiUrl = 'https://kind-rose-earthworm-hose.cyclic.app'
 
 export default apiUrl;
